428a6ff012cb5d9cb1c2d6d06bcd1b44e690f3f1
Anna <anna.yafi@datadoghq.com>
2022-06-03 15:17:03 +0200

    Adding benchmarks

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RE2CS;
+
+const string pattern = @"\[(.*?)\]((?:.\s*)*?)\[\/\1\]";
+
+Console.WriteLine("Hello, World!");
+var re = new RE2(pattern);
+var m = re.Match("123-123-1234");
